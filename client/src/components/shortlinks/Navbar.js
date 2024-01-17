3cab3ad86caf79f4f8be504936555a5e6927a7df
ddsdharan <128017667+ddsdharan@users.noreply.github.com>
2024-01-17 18:47:47 +0530

    Update Navbar.js

--- client/src/components/shortlinks/Navbar.js
+++ client/src/components/shortlinks/Navbar.js
@@ -26,6 +26,7 @@ function Navbar() {
                 <button to={"/portal/shortlink"} className="btn navbar-brand col px-md-4" ><b>URL SHORTNER APPLICATION</b></button>
                 <Link to={"/portal/shortlink"} className="btn navbar-brand col px-md-4" >URL Shortner</Link>
                 <Link to={"/portal/listurl"} className="btn navbar-brand col px-md-4" >URL List</Link>
+                    <span className='owner'>Owned by: Sri Sujith</span>
             </div>
 
             <div className='col-lg-4 '>
