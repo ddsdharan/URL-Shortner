d895f3ad726dcf94d920a28ca9cbfd34fa1628f2
ddsdharan <128017667+ddsdharan@users.noreply.github.com>
2024-01-22 22:30:39 +0530

    Update listUrl.js

--- client/src/components/shortlinks/listUrl.js
+++ client/src/components/shortlinks/listUrl.js
@@ -1,67 +1,61 @@
 import axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { toast } from "react-toastify";
 import { config } from "../../config";
-import UserContext from '../../context/UserContext';
-
 import ListTable from "./ListTable";
 
 function ListUrl() {
+    let mail = localStorage.getItem('email');
+    const initialRows = [];
 
-  let mail = localStorage.getItem('email');;
+    const [rows, setRows] = useState(initialRows);
 
-  const initialRows = [
+    const columns = [
+        "longurl",
+        "shorturl",
+        "clicked count"
+    ];
 
-  ];
-  const [rows, setRows] = useState(initialRows);
+    // Define getList using useCallback
+    const getList = useCallback(async () => {
+        try {
+            const url = await axios.get(`${config.api}/link/geturls/${mail}`, {
+                headers: {
+                    'Authorization': `${localStorage.getItem('token')}`
+                }
+            });
 
-  const columns = [
-    "longurl",
-    "shorturl",
-    "clicked count"
-  ];
+            if (url) {
+                const formattedArray = url.data.newData.map((obj) => {
+                    return {
+                        longurl: obj.longurl,
+                        clickedcount: obj.clickedcount,
+                        shorturl: obj.shorturl
+                    };
+                });
 
-
-  const getList = async () => {
-    try {
-      const url = await axios.get(`${config.api}/link/geturls/${mail}`, {
-        headers: {
-          'Authorization': `${localStorage.getItem('token')}`
+                setRows(formattedArray);
+            } else {
+                toast.error(url.data.message);
+            }
+        } catch (error) {
+            console.log(error);
         }
-      });
+    }, [mail]);
 
+    useEffect(() => {
+        // Now you can use getList directly in the dependency array
+        getList();
+    }, [getList]);
 
-      if (url) {
-        const formattedArray = url.data.newData.map((obj) => {
-          return {
-            longurl: obj.longurl,
-            clickedcount: obj.clickedcount,
-            shorturl: obj.shorturl
-          };
-        });
-
-        setRows(formattedArray);
-      } else {
-        toast.error(url.data.message);
-      }
-    } catch (error) {
-      console.log(error)
-    }
-  }
-  useEffect(() => {
-    getList();
-  }, []);
-
-
-
-
-  return (
-    <>
-      <div className="list-url-container">
-        list url page
-        <ListTable rows={rows} columns={columns} />
-      </div>
-    </>
-  );
+    return (
+        <>
+            <div className="list-url-container">
+                list url page
+                <ListTable rows={rows} columns={columns} />
+            </div>
+        </>
+    );
 }
+
 export { ListUrl };
